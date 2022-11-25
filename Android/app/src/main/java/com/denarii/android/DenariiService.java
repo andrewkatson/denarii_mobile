3ce287b08f803a24c8785d8a2f41edb72c308b94
Andrew Katson <katsonandrew3.5@gmail.com>
2022-11-25 12:52:27 -0500

    Update wallet details and network interface
    Add in the seed as a wallet detail and add in email as part of the different interface calls.

--- Android/app/src/main/java/com/denarii/android/DenariiService.java
+++ Android/app/src/main/java/com/denarii/android/DenariiService.java
@@ -8,18 +8,18 @@ import retrofit2.http.Path;
 
 public interface DenariiService {
 
-    @POST("users/{user}/create")
-    Call<Wallet> createWallet(@Path("user") String userName);
+    @POST("users/{user}/{email}/{password}/create")
+    Call<Wallet> createWallet(@Path("user") String userName, @Path("email") String email, @Path("password") String password);
 
-    @PATCH("users/{user}/{wallet}/{password}/{seed}/restore")
-    Call<Wallet> restoreWallet(@Path("user") String userName, @Path("wallet") String walletName, @Path("password") String password, @Path("seed") String seed);
+    @PATCH("users/{user}/{email}/{wallet}/{password}/{seed}/restore")
+    Call<Wallet> restoreWallet(@Path("user") String userName, @Path("email") String email, @Path("wallet") String walletName, @Path("password") String password, @Path("seed") String seed);
 
-    @GET("users/{user}/{wallet}/{password}/open")
-    Call<Wallet> openWallet(@Path("user") String userName, @Path("wallet") String walletName, @Path("password") String password);
+    @GET("users/{user}/{email}/{wallet}/{password}/open")
+    Call<Wallet> openWallet(@Path("user") String userName, @Path("email") String email, @Path("wallet") String walletName, @Path("password") String password);
 
-    @GET("users/{user}/{wallet}/balance")
-    Call<Wallet> getBalance(@Path("user") String userName, @Path("wallet") String walletName);
+    @GET("users/{user}/{email}/{wallet}/balance")
+    Call<Wallet> getBalance(@Path("user") String userName, @Path("email") String email, @Path("wallet") String walletName);
 
-    @GET("users/{user}/{wallet}/{amount}/send")
-    Call<Wallet> sendDenarii(@Path("user") String userName, @Path("wallet") String walletName, @Path("amount") int amountToSend);
+    @POST("users/{user}/{email}/{wallet}/{amount}/send")
+    Call<Wallet> sendDenarii(@Path("user") String userName, @Path("email") String email, @Path("wallet") String walletName, @Path("amount") int amountToSend);
 }
--- Android/app/src/main/java/com/denarii/android/WalletDetails.java
+++ Android/app/src/main/java/com/denarii/android/WalletDetails.java
@@ -2,4 +2,5 @@ package com.denarii.android;
 
 public class WalletDetails {
     public double balance;
+    public String seed;
 }
