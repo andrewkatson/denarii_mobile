2138b4bdbd2f0474224701f8b4197cec4f6eb9c2
Andrew Katson <katsonandrew3.5@gmail.com>
2022-11-28 00:45:15 -0500

    Delete secret key
    This is just a play one but still never have it on the repo

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a$bxvk!a(o0-wx45ach8+rm1qa8rc)(4y%u50$v9urn+eej(vh'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
