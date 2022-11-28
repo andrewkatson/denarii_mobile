d43c4247d49a6ddc16cb38813232da1c2366d1fc
Andrew Katson <katsonandrew3.5@gmail.com>
2022-11-28 00:56:40 -0500

    Script to generate secret keys and read secret key from file

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -18,9 +18,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
+# SECURITY WARNING: keep the secret key used in production secret!
+with open(Path(f"{BASE_DIR}/Backend/secret_key.txt"), "rb") as secret_key_file:
+    lines = secret_key_file.readlines()
+    for line in lines:
+        SECRET_KEY = line
+        break
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
