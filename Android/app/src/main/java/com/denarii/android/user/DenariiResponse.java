5f47c14301483b52fc0cd77f09997e6f24dd200a
Drew Glinsman <31940539+DrewGlinsman@users.noreply.github.com>
2023-07-26 16:27:38 -0400

    Update DenariiResponse.java

--- Android/app/src/main/java/com/denarii/android/user/DenariiResponse.java
+++ Android/app/src/main/java/com/denarii/android/user/DenariiResponse.java
@@ -35,4 +35,31 @@ public class DenariiResponse implements Serializable {
 
     @SerializedName("transaction_was_settled")
     public boolean transactionWasSettled;
+
+    @SerializedName("verification_status")
+    public String verificationStatus;
+
+    @SerializedName("author")
+    public String author;
+
+    @SerializedName("content")
+    public String content;
+
+    @SerializedName("description")
+    public String description;
+
+    @SerializedName("title")
+    public String title;
+
+    @SerializedName("support_ticket_id")
+    public String supportTicketID;
+
+    @SerializedName("is_resolved")
+    public boolean isResolved;
+
+    @SerializedName("creation_time_body")
+    public String creationTimeBody;
+
+    @SerializedName("updated_time_body")
+    public String updatedTimeBody;
 }
