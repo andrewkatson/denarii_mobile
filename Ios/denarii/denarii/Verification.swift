bba4420adc92af8a44d1674c112b3cdd8d541cc9
andrewkatson <katsonandrew3.5@gmail.com>
2023-11-04 10:03:33 -0400

    Verification screen done

--- Ios/denarii/denarii/Verification.swift
+++ Ios/denarii/denarii/Verification.swift
@@ -19,7 +19,7 @@ struct Verification: View {
     @State private var workCity: String = ""
     @State private var workState: String = ""
     @State private var workCountry: String = ""
-    @State private var status: String = ""
+    @State private var status: String = "Status: Not Verified Yet"
     
     @State private var isSubmitted: Bool = false
     @State private var showingPopover = false
@@ -47,8 +47,34 @@ struct Verification: View {
                 
                 let api = Config.api
                 
+                let responses = api.isAVerifiedPerson(Int(self.user.getValue().userID)!)
                 
+                if responses.isEmpty {
+                    status = formatStatus("unknown")
+                } else {
+                    let response = responses.first!
+                    
+                    status = formatStatus(response.verificationStatus)
+                }
             }
+        }
+    }
+    
+    func formatStatus(_ status: String) -> String {
+        if status == "is_verified" {
+            return "Status: Verified"
+        }
+        else if status == "failed_verification" {
+            return "Status: Failed Verification"
+        }
+        else if status == "verification_pending" {
+            return "Status: Verification Pending"
+        }
+        else if status == "is_not_verified" {
+            return "Status: Not Verified Yet"
+        }
+        else {
+            return "Status: Unknown"
         }
     }
 
@@ -56,27 +82,29 @@ struct Verification: View {
         VStack(alignment: .center) {
             Text("Verification").font(.largeTitle)
             Spacer()
-            Text("Status: \(status)")
-            TextField("First Name", text: $firstName)
-            TextField("Middle Initial", text: $middleName)
-            TextField("Last Name", text: $lastName)
-            TextField("Email", text: $email)
-            TextField("Date of Birth", text: $dob)
-            TextField("Social Security Number", text: $ssn)
-            TextField("Zipcode", text: $zipcode)
-            TextField("Phone Number", text: $phone)
-            TextField("Work City", text: $workCity)
-            TextField("Work State", text: $workState)
-            TextField("Work Country", text: $workCountry)
-            Button("Submit") {
-                isSubmitted = attemptSubmit()
-                showingPopover = true
-            }.popover(isPresented: $showingPopover) {
-                Text(successOrFailure.getValue())
-                    .font(.headline)
-                    .padding().onTapGesture {
-                        showingPopover = false
-                    }.accessibilityIdentifier("Popover")
+            Text("\(status)")
+            if !status.contains("Status: Verfiied") {
+                TextField("First Name", text: $firstName)
+                TextField("Middle Initial", text: $middleName)
+                TextField("Last Name", text: $lastName)
+                TextField("Email", text: $email)
+                TextField("Date of Birth", text: $dob)
+                TextField("Social Security Number", text: $ssn)
+                TextField("Zipcode", text: $zipcode)
+                TextField("Phone Number", text: $phone)
+                TextField("Work City", text: $workCity)
+                TextField("Work State", text: $workState)
+                TextField("Work Country", text: $workCountry)
+                Button("Submit") {
+                    isSubmitted = attemptSubmit()
+                    showingPopover = true
+                }.popover(isPresented: $showingPopover) {
+                    Text(successOrFailure.getValue())
+                        .font(.headline)
+                        .padding().onTapGesture {
+                            showingPopover = false
+                        }.accessibilityIdentifier("Popover")
+                }
             }
             Spacer()
             HStack {
@@ -101,7 +129,33 @@ struct Verification: View {
     }
     
     func attemptSubmit() -> Bool {
-        return false
+        if Constants.DEBUG {
+            self.successOrFailure.setValue("Successfully verified in DEBUG mode")
+            return true
+        } else {
+            
+            if self.user.getValue().userID.isEmpty {
+                successOrFailure.setValue("Failed to verify.")
+                return false
+            }
+            
+            let api = Config.api
+            
+            let responses = api.verifyIdentity(Int(self.user.getValue().userID)!, firstName, middleName, lastName, email, dob, ssn, zipcode, phone, formatWorkLocations(workCity, workState, workCountry))
+            
+            if responses.isEmpty {
+                successOrFailure.setValue("Could not verify identity server error")
+                return false
+            }
+                       
+            successOrFailure.setValue("Successfully applied for verification. See status for details.")
+            return true
+        }
+    }
+    
+    func formatWorkLocations(_ city: String, _ state: String, _ country: String) -> String {
+        
+        return "[{'country': \(country), 'state': \(state), 'city': \(city)}]"
     }
 }
 
